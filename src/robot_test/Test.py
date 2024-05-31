dfef83f676c44a42d7c6ef090e6a25c0e7ddc5c7
matteo <matteo@delgrosso.de>
2024-05-31 17:54:33 +0200

    Add division tests

--- src/robot_test/Test.py
+++ src/robot_test/Test.py
@@ -1,7 +1,13 @@
 import logging
 import control
+from enum import Enum, auto
 
 logger = logging.getLogger(f"{__name__} Logger")
+
+
+class Display(Enum):
+    GERMAN = auto()
+    ENGLISH = auto()
 
 
 def test_addition(num_left: int, num_right: int, exp_result: int) -> None:
@@ -13,7 +19,7 @@ def test_addition(num_left: int, num_right: int, exp_result: int) -> None:
     if not _enter_number(num_right):
         raise ValueError
     control.click(locator="id:equalButton type:button")
-    result: int = _get_result()
+    result: float = _get_result(display=Display.GERMAN)
     if result != exp_result:
         raise AssertionError(f"{result} != {exp_result}")
 
@@ -27,7 +33,7 @@ def test_subtraction(num_left: int, num_right: int, exp_result: int) -> None:
     if not _enter_number(num_right):
         raise ValueError
     control.click(locator="id:equalButton type:button")
-    result: int = _get_result()
+    result: float = _get_result(display=Display.GERMAN)
     if result != exp_result:
         raise AssertionError(f"{result} != {exp_result}")
 
@@ -52,44 +58,85 @@ def test_multiplication(num_left: int, num_right: int, exp_result: int) -> None:
     if not _enter_number(num_right):
         raise ValueError
     control.click(locator="id:equalButton type:button")
-    result: int = _get_result()
+    result: float = _get_result(display=Display.GERMAN)
     if result != exp_result:
         raise AssertionError(f"{result} != {exp_result}")
 
 
-def test_fail(arg: str) -> None:
-    """Failure test"""
-    logger.info(f"Called: {__name__}({arg})")
-    raise AssertionError
+def test_division() -> None:
+    """Division Test
+
+    Args:
+        num_left (int): _description_
+        num_right (int): _description_
+        exp_result (int): _description_
+
+    Raises:
+        ValueError: _description_
+        ValueError: _description_
+        AssertionError: _description_
+    """
+    test_params: tuple[tuple[int, int, float], ...] = (
+        (1, 2, 0.5),
+        (10, 2, 5),
+        (0, 2, 0),
+        (102, 2, 51),
+    )
+    for num_left, num_right, exp_result in test_params:
+        logger.info(f"{__name__}({num_left}, {num_right})")
+        if not _enter_number(num_left):
+            raise ValueError
+        control.click(locator="id:divideButton type:button")
+        if not _enter_number(num_right):
+            raise ValueError
+        control.click(locator="id:equalButton type:button")
+        result: float = _get_result(display=Display.GERMAN)
+        if result != exp_result:
+            raise AssertionError(f"{result} != {exp_result}")
 
 
-def _get_result() -> int:
+def _get_result(display: Display) -> float:
+    val: str = _get_result_as_string()
+    if display == Display.GERMAN:
+        thousand_sep = "."
+        decimal_sep = ","
+        val = val.replace(thousand_sep, "").replace(decimal_sep, ".")
+    elif display == Display.ENGLISH:
+        thousand_sep = ","
+        val = val.replace(thousand_sep, "")
+    # remove thousand separator
+    return float(val)
+
+
+def _get_result_as_string() -> str:
     result_display: str = control.get_attribute(
         locator="id:CalculatorResults", attribute="Name"
     )
-    logger.info(f'Result: "{result_display}"')
-    val: str = result_display.removeprefix("Display is ")
-    # remove thousand separator
-    return int(val.replace(".", ""))
+    logger.info(f'Result Display: "{result_display}"')
+    return result_display.removeprefix("Display is ")
 
 
-def _enter_number(num: int) -> bool:
+def _enter_number(num: float) -> bool:
     try:
         num_str = str(num)
     except ValueError:
         return False
 
-    is_negative = False
-    for pos, digit in enumerate(num_str):
+    decimal_separator = "."  # a decimla point denotes a decimal number
+    minus_sign = "-"  # a leading - denotes a negative number
+    plus_sign = "+"  # a leading + denotes a positive number
+
+    number_sign = num_str[0]
+    signed: bool = number_sign in (minus_sign, plus_sign)
+
+    for digit in num_str[1 if signed else 0 :]:
         if digit.isdigit():
             control.click(locator=f"id:num{digit}Button type:button")
-        elif digit == ".":
+        elif digit == decimal_separator:
             control.click(locator="id:decimalSeparatorButton type:button")
-        elif pos == 0 and digit == "-":
-            is_negative = True
         else:
             return False
-    if is_negative:
+    if signed and number_sign == minus_sign:
         control.click(locator="id:negateButton type:button")
 
     return True
--- src/robot_test/calculator_tests.robot
+++ src/robot_test/calculator_tests.robot
@@ -1,4 +1,5 @@
 *** Settings ***
+Documentation    Test Windows Calculator
 Test Tags    math
 Library    Application.py
 Library    Test.py
@@ -6,32 +7,40 @@ Library    Test.py
 Suite Setup    Start Application    start_parameter
 Suite Teardown    Stop Application
 Test Setup    Init Application
+Test Timeout    2 minutes
+
 
 *** Test Cases ***
 Test Addition
     [Documentation]    Verify addition
-    Test Addition    4    4    8
-    Test Addition    90    8    98
-    Test Addition    0100    0    100
-    Test Addition    9    92    101
-    Test Addition    698    -92    606
-    Test Addition    3214    3214    6428
+    [Template]    Test Addition
+    num_left=4    num_right=-5    exp_result=-1
+    num_left=90    num_right=-8    exp_result=82
+    num_left=0100    num_right=0    exp_result=100
+    num_left=-9    num_right=92    exp_result=83
+    num_left=698    num_right=92    exp_result=790
+    num_left=3214    num_right=3214    exp_result=6428
 
 Test Subtraction
     [Documentation]    Verify subtraction
-    Test Subtraction    9    3    6
-    Test Subtraction    178    1    177
-    Test Subtraction    0    7    -7
-    Test Subtraction    -6    -5    -1
-    Test Subtraction    -6003    1    -6004
+    [Template]    Test Subtraction
+    9    3    6
+    178    1    177
+    0    7    -7
+    -6    -5    -1
+    -6003    1    -6004
 
 Test Multiplication
     [Documentation]    Verify multiplication
-    Test Multiplication    1    5    5
-    Test Multiplication    10    5    50
-    Test Multiplication    -1    79    -79
-    Test Multiplication    -41    -79    3239
-    Test Multiplication    0    25    0
-    Test Multiplication    0    0    0
-    Test Multiplication    10000    1    10000
+    [Template]    Test Multiplication
+    1    5    5
+    10    5    50
+    -1    79    -79
+    -41    -79    3239
+    0    25    0
+    0    0    0
+    10000    1    10000
 
+Test Division
+    [Documentation]    Verify division
+    Test Division
